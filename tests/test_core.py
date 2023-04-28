6e891c5a8bdfbfcbccc3707c0d8281a9a79808b7
james.marcogliese@gmail.com <james.marcogliese@gmail.com>
2023-04-28 12:10:53 -0400

    Initial configuration.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,23 +1,16 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
-
 from singer_sdk.testing import get_tap_test_class
-
 from tap_openexchangerates.tap import Tapopenexchangerates
 
-
 SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+    "date": "2023-04-23",
+    "app_id": "REPLACE_WITH_YOUR_APP_ID",
 }
-
 
 # Run standard built-in tap tests from the SDK:
 TestTapopenexchangerates = get_tap_test_class(
     tap_class=Tapopenexchangerates,
-    config=SAMPLE_CONFIG
+    config=SAMPLE_CONFIG,
+    custom_suites=[]
 )
-
-
-# TODO: Create additional tests as appropriate for your tap.
