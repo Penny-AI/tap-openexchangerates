f89ac71975b82acfe8c2ce5f525bbae4e3a4d054
james.marcogliese@gmail.com <james.marcogliese@gmail.com>
2023-05-03 14:20:03 -0400

    Added working standard tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,14 +1,22 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
+from __future__ import annotations
+
+import responses
+from responses import _recorder
+import pytest
+import re
+import json
 from singer_sdk.testing import get_tap_test_class
 from tap_openexchangerates.tap import Tapopenexchangerates
+from singer_sdk.testing import get_standard_tap_tests
 from singer_sdk.testing.suites import TestSuite
 from .test_config import (
     StreamConfigurationTest,
     StreamParseResponseTest,
     StreamGetUrlTest,
-    TapChildStreamTest,
-    TapConfigTest)
+    TapConfigTest,
+    TapChildStreamTest)
 
 SAMPLE_CONFIG = {
     "start_date": "2023-04-23",
@@ -35,9 +43,52 @@ tap_tap_test_suite = TestSuite(
     ]
 )
 
-# Run standard built-in tap tests from the SDK:
-TestTapopenexchangerates = get_tap_test_class(
-    tap_class=Tapopenexchangerates,
-    config=SAMPLE_CONFIG,
-    custom_suites=[tap_stream_test_suite, tap_tap_test_suite]
-)
+
+@pytest.fixture()
+def historical_response():
+    """Return a sample response for the historical stream."""
+    return {"disclaimer": "",
+            "license": "",
+            "timestamp": 1682207982,
+            "base": "USD",
+            "rates": {"ZWL": 322.12}}
+
+
+@responses.activate
+def test_standard_tap_tests(historical_response: dict):
+    """Run standard tap tests from the SDK."""
+    responses.add_passthru(re.compile("https://raw.githubusercontent.com/\\w+"))
+
+    responses.add(
+        responses.GET,
+        re.compile("https://openexchangerates.org/api/historical.*"),
+        json=historical_response,
+        status=200,
+    )
+
+    tests = get_standard_tap_tests(Tapopenexchangerates, config=SAMPLE_CONFIG)
+    for test in tests:
+        test()
+
+
+with responses.RequestsMock() as rsps:
+    rsps.add_passthru(re.compile("https://raw.githubusercontent.com/\\w+"))
+    rsps.add(
+        responses.GET,
+        re.compile(".*"),
+        json=json.dumps({"disclaimer": "",
+                "license": "",
+                "timestamp": 1682207982,
+                "base": "USD",
+                "rates": {"ZWL": 322.12}}),
+        status=200
+    )
+
+    TestTapopenexchangerates = get_tap_test_class(
+        tap_class=Tapopenexchangerates,
+        config=SAMPLE_CONFIG,
+        custom_suites=[tap_stream_test_suite, tap_tap_test_suite],
+        include_tap_tests=False,
+        include_stream_tests=False,
+        include_stream_attribute_tests=False,
+    )
